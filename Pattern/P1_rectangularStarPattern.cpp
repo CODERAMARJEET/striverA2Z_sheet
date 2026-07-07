accb544279f3febe07bf6786e26d1b0a2268767f
Amarjeet Kumar <amarjeet4221official@gmail.com>
2026-07-07 21:45:44 +0530

    Add rectangular star pattern solution

---
+++ Pattern/P1_rectangularStarPattern.cpp
@@ -0,0 +1,26 @@
+/*
+Q1. Pattern 1:
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+Solve By --Amarjeet Kumar
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 0;
+    cout<<"Enter the value of n:\n";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
